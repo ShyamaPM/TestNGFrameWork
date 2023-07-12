5b784e6edcbf5b9f00168621eb33600f9760c06c
ShyamaPM <shyamapm94@gmail.com>
2023-07-12 23:20:56 +0530

    tenth commit

---
+++ src/test/java/com/obsqura/TestNGframework/DropDown.java
@@ -0,0 +1,62 @@
+package com.obsqura.TestNGframework;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.Test;
+
+public class DropDown extends Base
+{
+	@Test
+	public void dropDownUsingSelectClass()
+	{
+		driver.navigate().to("https://selenium.obsqurazone.com/select-input.php");
+		WebElement dropDown = driver.findElement(By.xpath("//select[@id='single-input-field']"));
+		Select select = new Select(dropDown);
+		select.selectByVisibleText("Red");
+		select.selectByValue("Yellow");
+		select.selectByIndex(3);
+	}
+	
+	@Test
+	public void selectingLnaguageFromSeleniumSite()
+	{
+		String languageName = "Português (Brasileiro)";
+		driver.navigate().to("https://www.selenium.dev/");
+		List <WebElement> languages = driver.findElements(By.xpath("//ul[@class='dropdown-menu']//a"));
+		WebElement dropDown = driver.findElement(By.xpath("//a[text()='English']"));
+		dropDown.click();
+		for(WebElement languageList: languages)
+		{
+			String menuText = languageList.getText();
+			if(menuText.equals(languageName))
+			{
+				languageList.click();
+				break;
+			}
+		}
+		
+	}
+	
+	@Test
+	public void selectingAboutDropDownValue()
+	{
+		String aboutName= "Structure and Governance";
+		driver.navigate().to("https://www.selenium.dev/");
+		List <WebElement> about = driver.findElements(By.xpath("//div[@class='dropdown-menu']//a"));
+		WebElement dropDownValue = driver.findElement(By.xpath("//a[@id='navbarDropdown']"));
+		dropDownValue.click();
+		for(WebElement aboutDropDown: about)
+		{
+			String menuText = aboutDropDown.getText();
+			if(menuText.equals(aboutName))
+			{
+				aboutDropDown.click();
+				break;
+			}
+		}
+	}
+
+}
---
+++ src/test/java/com/obsqura/TestNGframework/MenuOptions.java
@@ -0,0 +1,27 @@
+package com.obsqura.TestNGframework;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+public class MenuOptions extends Base
+{
+	@Test
+	public void menuSelection()
+	{
+		String menuCategory = "Checkbox Demo";
+		List <WebElement> menu = driver.findElements(By.xpath("//li[@class='list-group-item']//a"));
+		for(WebElement menuOption:menu)
+		{
+			String menuText= menuOption.getText();
+			if(menuText.equals(menuCategory))
+			{
+				menuOption.click();
+				break;
+			}
+		}
+	}
+
+}
